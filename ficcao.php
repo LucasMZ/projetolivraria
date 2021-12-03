e1899fd20c8a2ab1ec0b6b689163684cf50a57b2
LucasMZ <92659220+LucasMZ@users.noreply.github.com>
2021-12-03 13:26:56 -0300

    Add files via upload

---
+++ ficcao.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html class="fundo-2">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="/flexbox.css">
+        
+    </head>
+    <body>
+        <?php
+            include __DIR__ . "/include/navegacao.php";
+        ?> 
+       <section class="container-2">
+                <div class="box-2"><img src="doctorwho.PNG" height="260" width="270"><p class="paragrafo">Doctor Who: Hora nenhuma</p></div>
+                <div class="box-2"><img src="adancadamorte.PNG" height="260" width="270"><p class="paragrafo">A danca da morte</p></div>
+                <div class="box-2"><img src=""><p class="paragrafo">c</p></div>
+                <div class="box-2"><img src=""><p class="paragrafo">domin</p></div>
+        </section>
+        
+       
+    </body>
+</html>
---
+++ filosofia.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html class="fundo-4">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="./flexbox.css">
+        
+    </head>
+    <body>
+        <?php
+            include __DIR__ . "/include/navegacao.php";
+        ?>
+        <section class="container-2">
+<div class="box-1"><img src="./kant.PNG" height="260" width="270"><p class="paragrafo">Immanuel Kant: Crítica da razão prática</p></div>
+	<div class="box-1"><img src="descartes.PNG" height="260" width="270"><p class="paragrafo">Descartes: Discurso do metodo</p></div>
+		<div class="box-1"><img src="./republica.jpg" height="260" width="270"><p class="paragrafo">Platão: A República</p></div>
+			<div class="box-1"><img src="./oprincipe.jpg" height="260" width="270"><p class="paragrafo">Maquiavél: O Principe</p></div>		
+</section>
+    <br>
+    <br>
+<section class="container-2">
+<div class="box-1"><img src="./oestrangeiro.jpg" height="260" width="270"><p class="paragrafo">Albert Camus: O Estrangeiro</p></div>
+<div class="box-1"><img src="./niet.png" height="260" width="270"><p class="paragrafo">Nietszche: Assim Falou Zaratustra</p></div>
+<div class="box-1"><img src="./shopenhauer.jpg" height="260" width="270"><p class="paragrafo">Schopenhauer: Sobre os Sofrimentos do Mundo</p></div>
+<div class="box-1"><img src="./sofia.jpg" height="260" width="270"><p class="paragrafo">Jostein G: O Mundo de Sofia</p></div>
+</section>
+<br>
+<br>
+<section class="container-2">
+<div class="box-1"><img src="./banquete.jpg" height="260" width="270"><p class="paragrafo">Platão: O Banquete</p></div>
+<div class="box-1"><img src="./" height="260" width="270"><p class="paragrafo"></p></div>
+<div class="box-1"><img src="./" height="260" width="270"><p class="paragrafo"></p></div>
+<div class="box-1"><img src="./" height="260" width="270"><p class="paragrafo"></p></div>
+</section>
+        <?php
+            include __DIR__ . "/include/rodape.php";
+        ?>
+    </body>
+</html>
