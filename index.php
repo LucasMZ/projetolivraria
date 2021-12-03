9feeafbb6d33f3bcee901cb3069906e68f7ad127
LucasMZ <92659220+LucasMZ@users.noreply.github.com>
2021-12-03 13:26:15 -0300

    index
    arquivo principal

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html class="fundo-6">
+    <head>
+        <meta charset="UTF-8">
+        <title>Inicio</title>
+    <link rel="stylesheet" type="text/css" href="/flexbox.css">
+    </head>
+    <body>
+    <input class="searchbar" type="search" name="pesquisa"><button class="button"><img class="img" src="./search.png"></button>
+    <br>
+    <br>
+        <div class="item-10"><h1><a href="index.php" style="text-decoration: none;" ><img class="item-8" src="home.png" /></a></h1></div>
+        <div class="item-6"><h1><a href="classicos.php" style="text-decoration: none;">Classicos</a></h1></div>
+        <div class="item-6"><h1><a href="ficcao.php" style="text-decoration: none;">Ficcao Cientifica</a></h1></div>
+        <div class="item-6"><h1><a href="romance.php" style="text-decoration: none;">Romance</a></h1></div>
+        <div class="item-6"><h1><a href="filosofia.php" style="text-decoration: none;">filosofia</a></h1></div>
+        <div class="item-6"><h1><a href="historia.php" style="text-decoration: none;">Historia</a></h1></div>
+    
+    
+
+    </body>
+</html>
