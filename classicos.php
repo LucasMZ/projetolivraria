e955c17698c10a235808085a892883aeda23990e
LucasMZ <92659220+LucasMZ@users.noreply.github.com>
2021-12-03 13:27:52 -0300

    Add files via upload

---
+++ classicos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html class="fundo-4">
+    <head>
+        <meta charset="UTF-8">
+        <title>Classicos</title>
+        <link rel="stylesheet" type="text/css" href="./flexbox.css">
+        
+    </head>
+    <body>
+        <?php
+            include __DIR__ . "/include/navegacao.php";
+        ?>
+        <section class="container-2">
+<div class="box-1"><img src="./ocortico.jpg" height="260" width="270"><p class="paragrafo">O Cortiço</p></div>
+<div class="box-1"><img src="./domcasmurro.jpg" height="260" width="270"><p class="paragrafo">Dom Casmurro</p></div>
+<div class="box-1"><img src="./3mosqueteiros.jpg" height="260" width="270"><p class="paragrafo">Os Três mosqueteiros</p></div>
+<div class="box-1"> <img src="./mobydick.jpg" height="260" width="270"><p class="paragrafo">Moby Dick</p></div>
+</section>
+<br>
+<br>
+<section class="container-2">
+<div class="box-1"><img src="./romeuejulieta.jpg" height="260" width="270"><p class="paragrafo">Romeu e Julieta</p></div>
+<div class="box-1"><img src="./orgulhoepreconceito.jpg" height="260" width="270"><p class="paragrafo">Orgulho E Preconceito</p></div>
+<div class="box-1"><img src="./domquixote.jpg" height="260" width="270"><p class="paragrafo">Dom Quixote de la Mancha</p></div>
+<div class="box-1"><img src="./hamlet.jpg" height="260" width="270"><p class="paragrafo">Hamlet</p></div>
+</section>
+        
+    </body>
+</html>
