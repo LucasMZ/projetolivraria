33225525575819c07a34f0cb9c578ae8964e98fd
LucasMZ <92659220+LucasMZ@users.noreply.github.com>
2021-12-03 13:28:11 -0300

    Add files via upload

---
+++ historia.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html class="fundo-5">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="/flexbox.css">
+        
+    </head>
+    <body>
+        <?php
+            include __DIR__ . "/include/navegacao.php";
+        ?>
+       <section class="container-2">
+       <div class="box-5"><img src=""><p class="paragrafo">c</p></div>
+       <div class="box-5"><img src=""><p class="paragrafo">domin</p></div>
+       <div class="box-5"><img src=""><p class="paragrafo">c</p></div>
+       <div class="box-5"><img src=""><p class="paragrafo">domin</p></div>
+       </section>
+        
+        <?php
+            include __DIR__ . "/include/rodape.php";
+        ?>
+    </body>
+</html>
