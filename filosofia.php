13d824ea7c09caf2e1f5668dbc844053d6d96e08
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2022-02-05 11:57:01 -0300

    filosofia atualizado

--- filosofia.php
+++ filosofia.php
@@ -24,17 +24,17 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     <br>
 <section class="container-2">
 <div class="box-1"><img src="./oestrangeiro.jpg" height="260" width="270"><p class="paragrafo">Albert Camus: O Estrangeiro</p></div>
-<div class="box-1"><img src="./niet.png" height="260" width="270"><p class="paragrafo">Nietszche: Assim Falou Zaratustra</p></div>
-<div class="box-1"><img src="./shopenhauer.jpg" height="260" width="270"><p class="paragrafo">Schopenhauer: Sobre os Sofrimentos do Mundo</p></div>
-<div class="box-1"><img src="./sofia.jpg" height="260" width="270"><p class="paragrafo">Jostein G: O Mundo de Sofia</p></div>
+    <div class="box-1"><img src="./niet.png" height="260" width="270"><p class="paragrafo">Nietszche: Assim Falou Zaratustra</p></div>
+        <div class="box-1"><img src="./shopenhauer.jpg" height="260" width="270"><p class="paragrafo">Schopenhauer: Sobre os Sofrimentos do Mundo</p></div>
+            <div class="box-1"><img src="./sofia.jpg" height="260" width="270"><p class="paragrafo">Jostein G: O Mundo de Sofia</p></div>
 </section>
 <br>
 <br>
 <section class="container-2">
 <div class="box-1"><img src="./banquete.jpg" height="260" width="270"><p class="paragrafo">Platão: O Banquete</p></div>
-<div class="box-1"><img src="./soren.jpg" height="260" width="270"><p class="paragrafo">Soren Kierkgaard: O Desespero Humano</p></div>
-<div class="box-1"><img src="./wit.jpg" height="260" width="270">
-<p class="paragrafo">L Witgenstein: Tractatus Logico-Philosophicus</p>
+    <div class="box-1"><img src="./soren.jpg" height="260" width="270"><p class="paragrafo">Soren Kierkgaard: O Desespero Humano</p></div>
+        <div class="box-1"><img src="./wit.jpg" height="260" width="270">
+            <p class="paragrafo">L Witgenstein: Tractatus Logico-Philosophicus</p>
 <?php include __DIR__ . "./compra.php"; ?>
 </div>
 <div class="box-1"><img src="./ayn.jpg" height="260" width="270"><p class="paragrafo">Ayn Rand: A revolta de Atlas</p></div>
