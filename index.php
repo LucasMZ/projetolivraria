73e978b9ad9a48c4b61100fb8bed294323635c9b
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2022-02-05 11:55:43 -0300

    index atualizado

--- index.php
+++ index.php
@@ -8,22 +8,65 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <meta charset="UTF-8">
         <title>Inicio</title>
     <link rel="stylesheet" type="text/css" href="/flexbox.css">
+    
     </head>
     <body>
-    <input class="searchbar" type="search" name="pesquisa"><button class="button"><img class="img" src="./search.png"></button>
+    <?php include __DIR__ . "./include/navegacao.php"; ?>
     <br>
     <br>
-        <div class="item-10"><h1><a href="index.php" style="text-decoration: none;" ><img class="item-8" src="home.png" /></a></h1></div>
-        <div class="item-6"><h1><a href="classicos.php" style="text-decoration: none;">Classicos</a></h1></div>
-        <div class="item-6"><h1><a href="ficcao.php" style="text-decoration: none;">Ficcao Cientifica</a></h1></div>
-        <div class="item-6"><h1><a href="romance.php" style="text-decoration: none;">Romance</a></h1></div>
-        <div class="item-6"><h1><a href="filosofia.php" style="text-decoration: none;">filosofia</a></h1></div>
-        <div class="item-6"><h1><a href="historia.php" style="text-decoration: none;">Historia</a></h1></div>
-
-       <?php
-        var_dump($email);
-       ?>
-    
+        <section class="container-2">
+                <div class="box-1"><img src="doctorwho.PNG" height="260" width="270"><p class="paragrafo">Doctor Who: Hora nenhuma</p></div>
+                    <div class="box-1"><img src="adancadamorte.PNG" height="260" width="270"><p class="paragrafo">A danca da morte</p></div>
+                        <div class="box-1"><img src="lovecraft.jpg" height="260" width="270"><p class="paragrafo">Cthullu</p></div>
+                            <div class="box-1"><img src="20mil.jpg" height="260" width="265"> <p class="paragrafo">20 mil léguas submarinas</p></div>
+</section>
+        <br>
+        <section class="container-2">
+                <div class="box-1"><img src="lag.jpg" height="260" width="270"><p class="paragrafo">The Last Girl on Earth</p></div>
+                    <div class="box-1"><img src="neuro.jpG" height="260" width="270"><p class="paragrafo">Neuromancer</p></div>
+                        <div class="box-1"><img src="451.jpg" height="260" width="270"><p class="paragrafo">Fahrenheit 451</p></div>
+                            <div class="box-1"><img src="robo.jpg" height="260" width="265"> <p class="paragrafo">Eu, Robo</p></div>
+</section>
+<br>
+<br>
+<section class="container-2"> 
+<div class="box-1"><img src="./ocortico.jpg" height="260" width="270"><p class="paragrafo">O Cortiço</p></div>
+    <div class="box-1"><img src="./domcasmurro.jpg" height="260" width="270"><p class="paragrafo">Dom Casmurro</p></div>
+        <div class="box-1"><img src="./3mosqueteiros.jpg" height="260" width="270"><p class="paragrafo">Os Três mosqueteiros</p></div>
+            <div class="box-1"> <img src="./mobydick.jpg" height="260" width="270"><p class="paragrafo">Moby Dick</p></div>
+</section>
+<br>
+<section class="container-2"> 
+<div class="box-1"><img src="./romeuejulieta.jpg" height="260" width="270"><p class="paragrafo">Romeu e Julieta</p></div>
+    <div class="box-1"><img src="./orgulhoepreconceito.jpg" height="260" width="270"><p class="paragrafo">Orgulho E Preconceito</p></div>
+        <div class="box-1"><img src="./domquixote.jpg" height="260" width="270"><p class="paragrafo">Dom Quixote de la Mancha</p></div>
+            <div class="box-1"><img src="./hamlet.jpg" height="260" width="270"><p class="paragrafo">Hamlet</p></div>
+</section>
+<br>
+<section class="container-2"> 
+<div class="box-1"><img src="./kant.PNG" height="260" width="270"><p class="paragrafo">Immanuel Kant: Crítica da razão prática</p></div>
+	<div class="box-1"><img src="descartes.PNG" height="260" width="270"><p class="paragrafo">Descartes: Discurso do metodo</p></div>
+		<div class="box-1"><img src="./republica.jpg" height="260" width="270"><p class="paragrafo">Platão: A República</p></div>
+			<div class="box-1"><img src="./oprincipe.jpg" height="260" width="270"><p class="paragrafo">Maquiavél: O Principe</p></div>	
+</section>
+<br>
+<section class="container-2"> 
+<div class="box-1"><img src="./oestrangeiro.jpg" height="260" width="270"><p class="paragrafo">Albert Camus: O Estrangeiro</p></div>
+    <div class="box-1"><img src="./niet.png" height="260" width="270"><p class="paragrafo">Nietszche: Assim Falou Zaratustra</p></div>
+        <div class="box-1"><img src="./shopenhauer.jpg" height="260" width="270"><p class="paragrafo">Schopenhauer: Sobre os Sofrimentos do Mundo</p></div>
+            <div class="box-1"><img src="./sofia.jpg" height="260" width="270"><p class="paragrafo">Jostein G: O Mundo de Sofia</p></div>
+</section>
+<br>
+<br>
+<section class="container-2"> 
+<div class="box-1"><img src="./banquete.jpg" height="260" width="270"><p class="paragrafo">Platão: O Banquete</p></div>
+    <div class="box-1"><img src="./soren.jpg" height="260" width="270"><p class="paragrafo">Soren Kierkgaard: O Desespero Humano</p></div>
+        <div class="box-1"><img src="./wit.jpg" height="260" width="270"><p class="paragrafo">L Witgenstein: Tractatus Logico-Philosophicus</p>
+</section>
 
     </body>
+    <?php
+            include __DIR__ . "/include/rodape.php";
+        ?>
 </html>
+ 
